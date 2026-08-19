4cc2ce0a6d1b994d53e242a21d6078b72b771f30
Dca <pikirsendiri953@gmail.com>
2026-08-19 22:05:29 +0700

    produk 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    name: "MYKONOS ALL VARIAN DECANT",
+    name: "Decant Mykonos",
     description: "MYKONOS ALL VARIAN DECANT | 3ml 5ml 10ml ORIGINAL SHARE IN JAR",
     price: "Rp22.900",
     image: "https://down-id.img.susercontent.com/file/id-11134207-822wn-mo1m39jm2eit87@resize_w900_nl.webp",
@@ -16,11 +16,11 @@ const products = [
   },
 
   {
-    name: "Decant Mykonos",
-    description: "Decant Mykonos - 2ml / 3ml / 5ml / 10ml – Parfum Travel Size Original Wangi Tahan Lama",
-    price: "Rp120.650",
-    image: "https://down-id.img.susercontent.com/file/id-11134207-81zth-mq54dm2x3bwrbf@resize_w900_nl.webp",
-    link: "https://s.shopee.co.id/8V8GNFSYxB"
+    name: "Mykonos 50ML All Variant",
+    description: "Mykonos 50ML All Variant - Parfum Edp / Extrait De Parfum",
+    price: "Rp138.000",
+    image: "https://down-id.img.susercontent.com/file/id-11134207-81zti-mekt8rkuylmucb@resize_w900_nl.webp",
+    link: "https://s.shopee.co.id/9KhNOyAjm4"
   },
 
   {
